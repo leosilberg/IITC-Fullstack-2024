064774cf0d037093b97e247850179a76cd1460da
leosilberg <ariel.silberg@gmail.com>
2024-06-02 21:20:25 +0300

    Modules

--- Classwork/5.30/Modules/main.js
+++ Classwork/5.30/Modules/main.js
@@ -1,9 +1,13 @@
-import nums from "./module1.js";
+import {nums} from "./module1.js";
 import * as nums2 from "./module1.js";
 import c, { b as b1, f1, g } from "./module2.js";
 console.log(nums.a);
-console.log(nums2.default);
+console.log(nums2.nums.a);
 console.log(b1);
 console.log(c);
 console.log(f1);
 console.log(g);
+function dis(){
+    console.log("dfgn")
+}
+window.dis=dis
--- Classwork/5.30/Modules/module1.js
+++ Classwork/5.30/Modules/module1.js
@@ -1,4 +1,3 @@
 const a=20
 const d=30
-export const h=2
-export default {a,d};
+export const nums= {a,d};
